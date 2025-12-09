434d2efca95ebe3759c2cc30c5ea9fb4984db3a0
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-09 09:34:28 -0600

    Update 05_init.js

--- js/05_init.js
+++ js/05_init.js
@@ -1,34 +1,29 @@
 // 05_init.js
 import { loadData } from './02_data.js';
-// Importamos TODAS las funciones necesarias de render
-import { renderTurnoUI, renderOdometroUI, setupAdminListeners, renderDashboard, renderMetaDiaria, renderHistorial } from './03_render.js';
+// Importamos la nueva función
+import { renderTurnoUI, renderOdometroUI, setupAdminListeners, renderDashboard, renderMetaDiaria, renderHistorial, renderMantenimientoUI } from './03_render.js';
 import { initCharts } from './04_charts.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // 1. Cargar Datos Globales
     loadData();
-
-    // 2. Detectar Página
     const page = document.body.getAttribute('data-page');
-    console.log(`[Init] Sistema iniciado en vista: ${page}`);
 
-    // 3. Router de Vistas (EJECUCIÓN SELECTIVA)
-    
     if (page === 'admin') {
-        // SOLO ejecutas esto si estás en admin.html
         renderTurnoUI();      
         renderOdometroUI();   
-        renderMetaDiaria();   
-        setupAdminListeners(); // Aquí están los botones que fallaban
+        renderMetaDiaria();
+        
+        // ¡AQUÍ ESTÁ LA MAGIA!
+        // Ahora renderizamos mantenimiento de forma segura
+        renderMantenimientoUI();
+           
+        setupAdminListeners();
     } 
     else if (page === 'index') {
-        // SOLO ejecutas esto si estás en index.html
         renderDashboard(); 
         initCharts();         
     }
     else if (page === 'historial') {
-        // SOLO ejecutas esto si estás en historial.html
         renderHistorial(); 
     }
-    // Si añades tutorial, iría aquí...
 });
