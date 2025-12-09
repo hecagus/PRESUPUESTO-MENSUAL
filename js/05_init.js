8de92be0082e65c6b18aec629405c50d142cc2cd
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-08 20:29:05 -0600

    Update 05_init.js

--- js/05_init.js
+++ js/05_init.js
@@ -1,21 +1,19 @@
 // 05_init.js
 import { loadData } from './02_data.js';
-import { renderTurnoUI, renderOdometroUI, setupAdminListeners, renderDashboard, renderMetaDiaria, renderHistorial } from './03_render.js';
+// Importamos renderMantenimientoUI
+import { renderTurnoUI, renderOdometroUI, setupAdminListeners, renderDashboard, renderMetaDiaria, renderHistorial, renderMantenimientoUI } from './03_render.js'; 
 import { initCharts } from './04_charts.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // 1. Cargar Estado Global
     loadData();
-
-    // 2. Detectar Página
     const page = document.body.getAttribute('data-page');
     console.log(`[Init] Iniciando sistema en vista: ${page}`);
 
-    // 3. Router de Vistas
     if (page === 'admin') {
         renderTurnoUI();      
         renderOdometroUI();   
         renderMetaDiaria();   
+        renderMantenimientoUI(); // <-- Aseguramos que se llama al cargar
         setupAdminListeners();
     } 
     else if (page === 'index') {
@@ -23,6 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
         initCharts();         
     }
     else if (page === 'historial') {
-        renderHistorial(); // <-- ¡Llamada agregada!
+        renderHistorial(); 
     }
 });
