b85d0aa198d8e8711b5fc7efc75f474c98a59b11
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-17 12:41:43 -0600

    Update 05_init.js

--- js/05_init.js
+++ js/05_init.js
@@ -1,26 +1,23 @@
 import { loadData } from './02_data.js';
 import { 
     renderTurnoUI, renderOdometroUI, setupAdminListeners, 
-    renderMetaDiaria, renderMantenimientoUI, renderDashboard 
+    renderMetaDiaria, renderMantenimientoUI, renderDashboard, 
+    renderListasAdmin 
 } from './03_render.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    try {
-        loadData();
-        const page = document.body.getAttribute('data-page');
-        
-        if (page === 'admin') {
-            renderTurnoUI();
-            renderOdometroUI();
-            renderMetaDiaria();
-            renderMantenimientoUI();
-            setupAdminListeners();
-        } else if (page === 'index') {
-            renderDashboard();
-        }
-        console.log("App iniciada en:", page);
-    } catch (e) {
-        console.error("Fallo crítico en el inicio:", e);
+    loadData();
+    const page = document.body.getAttribute('data-page');
+
+    if (page === 'admin') {
+        renderTurnoUI();
+        renderOdometroUI();
+        renderMetaDiaria();
+        renderMantenimientoUI();
+        renderListasAdmin();
+        setupAdminListeners();
+    } else if (page === 'index') {
+        renderDashboard();
     }
 });
 
