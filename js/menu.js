ad3d583a7ded0356b6a25271a163ffa4705293e9
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-19 17:50:50 -0600

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,6 @@
+const btn = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.nav-menu');
 
+btn?.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
