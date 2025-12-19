824ef209979637fa8332f6380c5284a83270467e
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-19 17:16:23 -0600

    Update historial.js

--- js/historial.js
+++ js/historial.js
@@ -1,1 +1,6 @@
+import { load } from './02_data.js';
+import { fmtMoney } from './01_consts_utils.js';
 
+const d = load();
+document.getElementById('historial').innerHTML =
+  d.gastos.map(g => fmtMoney(g.monto)).join('<br>');
