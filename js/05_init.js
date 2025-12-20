f9ad85cdba2ea5aeb8b30faaccd8e8409c076d2c
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-20 17:43:23 -0600

    Update 05_init.js

--- js/05_init.js
+++ js/05_init.js
@@ -1,11 +1,19 @@
 // 05_init.js
 import { loadData } from "./02_data.js";
-import { renderDashboard, renderWallet } from "./03_render.js";
-import { initCharts } from "./04_charts.js";
+import {
+  renderGlobalMenu,
+  initAdminRender
+} from "./03_render.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    loadData();
-    renderDashboard();
-    renderWallet();
-    initCharts();
+  loadData();
+
+  // 🔥 MENÚ GLOBAL SIEMPRE
+  renderGlobalMenu();
+
+  const page = document.body.dataset.page;
+
+  if (page === "admin") {
+    initAdminRender();
+  }
 });
