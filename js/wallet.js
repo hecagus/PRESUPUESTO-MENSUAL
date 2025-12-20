e232aee262de998c674715b358d1877a8d0cf739
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-19 20:35:41 -0600

    Delete js/wallet.js

--- js/wallet.js
+++
@@ -1,6 +0,0 @@
-import { load } from './02_data.js';
-import { fmtMoney } from './01_consts_utils.js';
-
-const d = load();
-document.getElementById('walletInfo').innerHTML =
-  d.deudas.map(x => `${x.nombre}: ${fmtMoney(x.saldo)}`).join('<br>');
