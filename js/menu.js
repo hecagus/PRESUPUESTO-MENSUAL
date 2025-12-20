8b12c3c39f6ca7463fc20697c593e8093c3f4cc6
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-19 20:35:01 -0600

    Delete js/menu.js

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.nav-menu');
-
-btn?.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
