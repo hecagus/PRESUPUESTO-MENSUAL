b9d589c212432d3a5bc2da6dceacad75e47d1600
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-19 20:33:55 -0600

    Delete js/00_migrate.js

--- js/00_migrate.js
+++
@@ -1,9 +0,0 @@
-const OLD_KEY = 'PRESUPUESTO_DATA';
-const NEW_KEY = 'uber_tracker_data';
-
-if (!localStorage.getItem(NEW_KEY)) {
-  const old = localStorage.getItem(OLD_KEY);
-  if (old) {
-    localStorage.setItem(NEW_KEY, old);
-  }
-}
