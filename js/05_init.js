c205aafc7f6db667869b680e450a19a647866006
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-21 06:17:21 -0600

    Update 05_init.js

--- js/05_init.js
+++ js/05_init.js
@@ -1,18 +1,8 @@
 // 05_init.js
-import { loadAdminData } from "./02_data.js";
-import {
-  renderGlobalMenu,
-  initAdminRender,
-  initHistorialRender
-} from "./03_render.js";
+import { initAdminRender } from "./03_render.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  loadAdminData();
-
-  const page = document.body.dataset.page;
-
-  renderGlobalMenu();
-
-  if (page === "admin") initAdminRender();
-  if (page === "historial") initHistorialRender();
+  if (document.body.dataset.page === "admin") {
+    initAdminRender();
+  }
 });
