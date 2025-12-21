ff52ee3cb7977ff1aaa55995c4c66312425a9aab
hecagus <132156689+hecagus@users.noreply.github.com>
2025-12-21 06:47:39 -0600

    Update 05_init.js

--- js/05_init.js
+++ js/05_init.js
@@ -1,11 +1,6 @@
 // 05_init.js
-// =====================================
-// INIT GLOBAL — NO DEPENDE DE LA PÁGINA
-// =====================================
-
 import { renderGlobalMenu } from "./03_render.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // 🔒 El menú SIEMPRE se inicializa
   renderGlobalMenu();
 });
