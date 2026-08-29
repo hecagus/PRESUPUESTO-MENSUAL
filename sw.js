5a0304d44acbc7a4e76f5e4c26f9ee8b5a317971
hecagus <132156689+hecagus@users.noreply.github.com>
2026-08-29 12:49:30 -0600

    chore(v2.6): incluir plataforma financiera en PWA

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='hecagus-finance-2.2.0-shell-v1';
+const CACHE='hecagus-finance-2.6.0-shell-v1';
 const APP_SHELL=[
   './','./index.html','./onboarding.html','./admin.html','./wallet.html','./stats.html','./historial.html','./calendar.html','./offline.html',
   './style.css','./manifest.webmanifest','./pwa-icon.svg',
-  './js/01_consts_utils.js','./js/02_data.js','./js/03_render.js','./js/04_charts.js','./js/05_init.js','./js/06_income_ui.js','./js/07_sync.js','./js/08_pwa.js','./js/10_onboarding.js','./js/11_savings_goals.js','./js/12_savings_ui.js','./js/13_financial_life.js','./js/14_calendar_ui.js','./js/firebase-config.js'
+  './js/01_consts_utils.js','./js/02_data.js','./js/03_render.js','./js/04_charts.js','./js/05_init.js','./js/06_income_ui.js','./js/07_sync.js','./js/08_pwa.js','./js/10_onboarding.js','./js/11_savings_goals.js','./js/12_savings_ui.js','./js/13_financial_life.js','./js/14_calendar_ui.js','./js/15_accounts_engine.js','./js/16_forecast_engine.js','./js/17_automation_engine.js','./js/18_health_goals.js','./js/19_platform_ui.js','./js/firebase-config.js'
 ];
 
 self.addEventListener('install',event=>{
