65472fcd24842a6c00a33f04de9850f206938c8e
hecagus <132156689+hecagus@users.noreply.github.com>
2026-08-31 14:57:26 -0600

    test(home): validar módulos v2.8 y cache PWA

--- tests/syntax-v26.test.js
+++ tests/syntax-v26.test.js
@@ -6,7 +6,7 @@ import { readFileSync } from 'node:fs';
 const files=[
   'js/05_init.js','js/07_sync.js','js/15_accounts_engine.js','js/16_forecast_engine.js',
   'js/17_automation_engine.js','js/18_health_goals.js','js/19_platform_ui.js','js/20_home_engine.js',
-  'js/21_financial_life_v27.js','js/22_home_ui.js','sw.js'
+  'js/21_financial_life_v27.js','js/22_home_ui.js','js/23_home_semantics.js','js/24_home_ui_v28.js','sw.js'
 ];
 
 test('módulos financieros tienen sintaxis JavaScript válida',()=>{
@@ -17,7 +17,7 @@ test('módulos financieros tienen sintaxis JavaScript válida',()=>{
 
 test('service worker cachea plataforma y Hogar',()=>{
   const sw=readFileSync('sw.js','utf8');
-  for(const name of ['15_accounts_engine.js','16_forecast_engine.js','17_automation_engine.js','18_health_goals.js','19_platform_ui.js','20_home_engine.js','21_financial_life_v27.js','22_home_ui.js'])assert.match(sw,new RegExp(name.replace('.','\\.')));
+  for(const name of ['15_accounts_engine.js','16_forecast_engine.js','17_automation_engine.js','18_health_goals.js','19_platform_ui.js','20_home_engine.js','21_financial_life_v27.js','22_home_ui.js','23_home_semantics.js','24_home_ui_v28.js'])assert.match(sw,new RegExp(name.replace('.','\\.')));
   assert.match(sw,/home\.html/);
-  assert.match(sw,/hecagus-finance-2\.7\.3-shell/);
+  assert.match(sw,/hecagus-finance-2\.8\.0-shell/);
 });
